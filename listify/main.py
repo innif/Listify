07218ec915c56cb02c09c5bbe4918528d684b196
Finn Harms <post@finn-harms.de>
2020-04-30 16:54:47 +0200

    added apiKey-file (without the key)

--- listify/main.py
+++ listify/main.py
@@ -1,2 +1,14 @@
+import spotipy
+import spotipy.util as util
+import json
+
+token = ""
+username = "finnyboy1234567"
+scope = "user-read-currently-playing app-remote-control streaming user-read-playback-state user-modify-playback-state user-read-currently-playing"
+
 def start():
-    pass
+    apiKey = json.load(open("apiKey.json"))
+    token = util.prompt_for_user_token(username, scope, apiKey['id'], apiKey['secret'], 'https://raw.githubusercontent.com/innif/Listify/master/response.txt')
+
+start()
+
